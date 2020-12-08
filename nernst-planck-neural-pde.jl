dfc14403ab1c18665afa76067deb414d8b867e7e
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2020-12-08 17:44:16 -0500

    Fix bug. Converges very slowly.

--- nernst-planck-neural-pde.jl
+++ nernst-planck-neural-pde.jl
@@ -38,24 +38,24 @@ using NeuralPDE, Flux, ModelingToolkit, GalacticOptim, Optim, DiffEqFlux
 @derivatives Dxx''~x
 
 # Electrochemical parameters
-t_max = 50.0 # s
+t_max = 0.01 #50.0 # s
 x_max = 0.01 # m
 H_anode_rate = 4.2 # mol m^-3 s^-1
-OH_cathode_rate = 5 # mol m^-3 s^-1
+OH_cathode_rate = 5.0 # mol m^-3 s^-1
 H_0 = 1e-4 # mol m^-3
 OH_0 = 1e-4 # mol m^-3
 φ_0 = 4 # V
-z_H = 1
+z_H = 1.0
 D_H = 9.31e-9 # m^2 s^-1
-z_OH = -1
+z_OH = -1.0
 D_OH = 5.26e-9 # m^2 s^−1 
 F = 96485.3415 # A s mol^-1
 R = 8.31 # kg m^2 K^-1 mol^-1 s^-2
-T = 298 # K
+T = 298.0 # K
 σ_0 = 0.2 # S m^-1
 k_wf = 1.5e8 # m^3 mol^-1 s^-1
 k_wb = 2.7e-5 # s^-1
-H2O = 55500 # mol m^-3
+H2O = 55500.0 # mol m^-3
 c_H = z_H * D_H * F / (R * T) 
 c_OH = z_OH * D_OH * F / (R * T) 
 
@@ -72,7 +72,7 @@ c_OH = z_OH * D_OH * F / (R * T)
 eqs = [
         Dt(H(t,x)) ~ Dxx(H(t,x)) + c_H * Dx(H(t,x)) * φ(t,x) + c_H * H(t,x) * Dxx(φ(t,x)) + k_wb * H2O - k_wf * H(t,x) * OH(t,x),
         Dt(OH(t,x)) ~ Dxx(OH(t,x)) + c_H * Dx(OH(t,x)) * φ(t,x) + c_H * OH(t,x) * Dxx(φ(t,x)) + k_wb * H2O - k_wf * H(t,x) * OH(t,x),
-        0. ~ σ_0 * Dxx(φ(t,x)),
+        0.0 ~ σ_0 * Dxx(φ(t,x))
       ]
 
 bcs = [ H(0,x) ~ H_0,
@@ -82,7 +82,7 @@ bcs = [ H(0,x) ~ H_0,
         Dx(OH(t,0)) ~ 0.0,
         φ(t,0) ~ φ_0,
         Dx(H(t,x_max)) ~ 0.0,
-        OH(t,x_max) ~ H_anode_rate * t,
+        OH(t,x_max) ~ OH_anode_rate * t,
         φ(t,x_max) ~ 0.0]
 
 # Space and time domains
@@ -109,7 +109,7 @@ cb = function (p,l)
     return false
 end
 
-res = GalacticOptim.solve(prob, Optim.BFGS(); cb = cb, maxiters=5000)
+res = GalacticOptim.solve(prob, Optim.BFGS(); cb = cb, maxiters=20000)
 
 phi = discretization.phi
 
